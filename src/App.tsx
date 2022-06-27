473d0b50684ead555d9118d616463f5b9c749908
Erick <kekierick15@gmail.com>
2022-06-27 16:26:25 -0300

    criando janela de transação

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
 import { Dashboard } from "./components/dashboard";
+import Modal from 'react-modal';
 import { Header } from "./components/header";
 import { GlobalStyle } from "./styles/global";
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
-function App() {
+Modal.setAppElement('#root');
+
+export function App() {
+  const [ isNewTransactionModalOpen, setIsNewTransactionModalOpen ] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+      setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+      setIsNewTransactionModalOpen(false);     
+    }
+
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={ handleOpenNewTransactionModal }/>
+
       <Dashboard />
+
+      <NewTransactionModal
+      isOpen={isNewTransactionModalOpen}
+      onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
+
     </>
   );
 }
