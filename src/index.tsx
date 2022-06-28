3af7f923426bd33f22befb569448f644b8f4c6c2
Erick <kekierick15@gmail.com>
2022-06-28 09:25:47 -0300

    listando transações e seeds

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,29 @@ createServer ({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          tittle: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          tittle: 'Aluguel',
+          type: 'withdraw',
+          category: 'casa',
+          amount: 1100,
+          createdAt: new Date('2021-02-14 11:00:00'),
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
